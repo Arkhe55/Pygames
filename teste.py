d8841f1b77bc12cda2b8efe60973a595ea16528e
Arkhe55 <Gabriel_4682@outlook.com>
2026-06-19 14:21:06 -0300

    teste

--- teste.py
+++ teste.py
@@ -1,10 +1,1 @@
-letras = set()
-
-while True:
-    letra = input("Digite uma letra: ")
-    letras.add(letra.lower())
-    
-    if "l" in letras:
-        print("PARABÉNS")
-        break
-    print(letras)
+print("Hellow")
